b77ae5cdef9bf986b6e6813ec458e011a4af2a69
Rohan Saroha <rohansaroha@Rohans-MacBook-Pro.local>
2022-03-02 10:21:22 +0530

    added react intl support

--- common/Typography/Typography.tsx
+++ common/Typography/Typography.tsx
@@ -1,10 +1,13 @@
 import React, { CSSProperties } from "react";
 import { HTMLAttributes } from "react";
-import styled, { css, StyledComponent } from "styled-components";
+import { FormattedMessage } from 'react-intl';
+import If from "common/If/If";
 
 type TypographyTypes = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "pre";
 
 interface Props extends HTMLAttributes<any> {
+  id:string;
+  values:any;
   /**
    * @property type
    * html element to render
@@ -32,76 +35,80 @@ interface Props extends HTMLAttributes<any> {
 
 export type TypographyProps = Props;
 
-const CommonTypographyCss = css<Props>`
-  ${({ color, fontWeight, fontSize, lineHeight, letterSpacing, theme }) => `
-    color: ${color || theme.colors.text1};
-    ${fontSize ? `font-size: ${fontSize};` : ""};
-    ${fontWeight ? `font-weight: ${fontWeight};` : ""};
-    ${lineHeight ? `line-height: ${lineHeight}px;` : ""};
-    ${letterSpacing ? `letter-spacing: ${letterSpacing}px` : ""};
-    :focus-visible {
-      outline: 1px solid ${theme.colors.primary2}; 
-    }
-    font-family: inherit;
-  `}
-`;
+// const CommonTypographyCss = css<Props>`
+//   ${({ color, fontWeight, fontSize, lineHeight, letterSpacing, theme }) => `
+//     color: ${color || theme.colors.text1};
+//     ${fontSize ? `font-size: ${fontSize};` : ""};
+//     ${fontWeight ? `font-weight: ${fontWeight};` : ""};
+//     ${lineHeight ? `line-height: ${lineHeight}px;` : ""};
+//     ${letterSpacing ? `letter-spacing: ${letterSpacing}px` : ""};
+//     :focus-visible {
+//       outline: 1px solid ${theme.colors.primary2}; 
+//     }
+//     font-family: inherit;
+//   `}
+// `;
 
-/**
- * @description Styled Components have to created before mount.
- *
- */
+// /**
+//  * @description Styled Components have to created before mount.
+//  *
+//  */
 
-const H1 = styled.h1<Props>`
-  ${CommonTypographyCss}
-`;
-const H2 = styled.h2`
-  ${CommonTypographyCss}
-`;
-const H3 = styled.h3`
-  ${CommonTypographyCss}
-`;
-const H4 = styled.h4`
-  ${CommonTypographyCss}
-`;
-const H5 = styled.h5`
-  ${CommonTypographyCss}
-`;
-const H6 = styled.h6`
-  ${CommonTypographyCss}
-`;
-const P = styled.p`
-  ${CommonTypographyCss}
-`;
-const PRE = styled.pre`
-  ${CommonTypographyCss}
-`;
+// const H1 = styled.h1<Props>`
+//   ${CommonTypographyCss}
+// `;
+// const H2 = styled.h2`
+//   ${CommonTypographyCss}
+// `;
+// const H3 = styled.h3`
+//   ${CommonTypographyCss}
+// `;
+// const H4 = styled.h4`
+//   ${CommonTypographyCss}
+// `;
+// const H5 = styled.h5`
+//   ${CommonTypographyCss}
+// `;
+// const H6 = styled.h6`
+//   ${CommonTypographyCss}
+// `;
+// const P = styled.p`
+//   ${CommonTypographyCss}
+// `;
+// const PRE = styled.pre`
+//   ${CommonTypographyCss}
+// `;
 
-const getElementByType = (type: Props["type"]) => {
-  switch (type) {
-    case "h1":
-      return H1;
-    case "h2":
-      return H2;
-    case "h3":
-      return H3;
-    case "h4":
-      return H4;
-    case "h5":
-      return H5;
-    case "h6":
-      return H6;
-    case "p":
-      return P;
-    case "pre":
-      return PRE;
-    default:
-      return P;
-  }
-};
+// const getElementByType = (type: Props["type"]) => {
+//   switch (type) {
+//     case "h1":
+//       return H1;
+//     case "h2":
+//       return H2;
+//     case "h3":
+//       return H3;
+//     case "h4":
+//       return H4;
+//     case "h5":
+//       return H5;
+//     case "h6":
+//       return H6;
+//     case "p":
+//       return P;
+//     case "pre":
+//       return PRE;
+//     default:
+//       return P;
+//   }
+// };
 
-const Typography = ({ type, text, ...rest }: Props) => {
-  const Element: StyledComponent<TypographyTypes, any> = getElementByType(type || "h3");
-  return <Element {...rest}>{text}</Element>;
+const Typography = ({ id,values,type, text, ...rest }: Props) => {
+  // const Element: StyledComponent<TypographyTypes, any> = getElementByType(type || "h3");
+  return (<div {...rest}>
+    <If condition={id} otherwise={text}>
+    <FormattedMessage id={id} values={values} />
+    </If>
+    </div>);
 };
 
 Typography.propTypes = {};
@@ -109,6 +116,7 @@ Typography.propTypes = {};
 Typography.defaultProps = {
   type: "h3",
   text: "",
+  values:{}
 };
 
 export default Typography;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store/index";
+import {IntlProvider} from 'react-intl';
+import messages from '../translation/en.json'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <IntlProvider messages={messages} locale="en" defaultLocale="en">
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
+      </IntlProvider>
     </>
   );
 }
